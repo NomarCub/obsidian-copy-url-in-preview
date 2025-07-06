bff1f81401ff5470d40eaa4a67c60b8d017424e1
NomarCub <nomarcub@gmail.com>
2025-07-06 21:37:57 +0200

    remove unused type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,8 @@
-import { CanvasNode, FileSystemAdapter } from "obsidian";
+import { CanvasNode } from "obsidian";
 import type { i18n } from "i18next";
 
 export interface ElectronWindow extends Window {
     WEBVIEW_SERVER_URL: string;
-}
-
-export interface FileSystemAdapterWithInternalApi extends FileSystemAdapter {
-    open(path: string): Promise<void>;
 }
 
 export interface CanvasNodeWithUrl extends CanvasNode {
