77bc04de819ffee761d6e6a69dd509b303c9e112
NomarCub <nomarcub@gmail.com>
2026-08-23 23:05:37 +0200

    fix obsidian lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,23 @@ import { defineConfig } from "eslint/config";
 import obsidianmd from "eslint-plugin-obsidianmd";
 import tseslint from "typescript-eslint";
 
-export default defineConfig({
-    files: ["**/*.{ts,mts,mjs}"],
-    extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.strictTypeChecked,
-        ...tseslint.configs.stylisticTypeChecked,
-    ],
-    plugins: { obsidianmd },
-    languageOptions: {
-        parserOptions: { projectService: true },
+export default defineConfig([
+    { ignores: ["main.js"] },
+    ...obsidianmd.configs.recommended,
+    {
+        files: ["**/*.{ts,mts,mjs}"],
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+        ],
+        languageOptions: {
+            parserOptions: { projectService: true },
+        },
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+            "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
+            "@typescript-eslint/no-non-null-assertion": "off",
+        },
     },
-    // @ts-expect-error -- Temporary fixes for v0.1.9., see https://github.com/obsidianmd/eslint-plugin/issues/90
-    rules: {
-        // eslint-disable-next-line @typescript-eslint/no-misused-spread
-        ...obsidianmd.configs?.["recommended"],
-        "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
-        "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
-        "@typescript-eslint/no-non-null-assertion": "off",
-    },
-});
+]);
